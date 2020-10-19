7f53576efd96b30491a2d49eb84e2fe61193a1e5
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-10-19 22:44:42 +0300

    Finish base framework functional

---
+++ src/core/utils.js
@@ -0,0 +1,7 @@
+// Pure functions (не взаимодействуют с глобальными переменными)
+export function capitalize(string) {
+  if (typeof string !== 'string') {
+    return ''
+  }
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
